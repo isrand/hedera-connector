a0e7972d7d34b90d97f25629e8c0c2026a4fc8a5
Isra Nebot <israel.nebot@gmail.com>
2023-07-12 16:26:01 +0200

    Add default values for wallet type

--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,3 +1,5 @@
+import { WalletType } from "../wallet/enums/WalletType";
+
 export class Configuration {
-  public static walletType: string = String(process.env['WALLET_TYPE']);
+  public static walletType: string = String(process.env['WALLET_TYPE']) || WalletType.FileSystem; // default to a filesystem wallet if the environment variable is not passed
 }
