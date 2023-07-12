34fc30874ff9a2d2f98c62e194b2147e96c7cfee
Isra Nebot <israel.nebot@gmail.com>
2023-07-12 15:11:40 +0200

    Remove unused files

--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,10 +1,3 @@
 export class Configuration {
-  public static hederaAccountId: string = String(process.env['HEDERA_ACCOUNT_ID']);
-  public static hederaPublicKey: string = String(process.env['HEDERA_PUBLIC_KEY']);
-  public static hederaPrivateKey: string = String(process.env['HEDERA_PRIVATE_KEY']);
-
-  public static kyberPublicKey: string = String(process.env['KYBER_PUBLIC_KEY']);
-  public static kyberPrivateKey: string = String(process.env['KYBER_PRIVATE_KEY']);
-
   public static walletType: string = String(process.env['WALLET_TYPE']);
 }
