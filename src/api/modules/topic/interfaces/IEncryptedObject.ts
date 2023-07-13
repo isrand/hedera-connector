87e174992a3e3aa5eea6d86270bfcf3987f0dfe3
Isra Nebot <israel.nebot@gmail.com>
2023-07-13 11:56:34 +0200

    Add explanation to field h

--- src/api/modules/topic/interfaces/IEncryptedObject.ts
+++ src/api/modules/topic/interfaces/IEncryptedObject.ts
@@ -20,5 +20,11 @@ export interface IEncryptedObject {
    */
   c: Array<string>;
 
+  /*
+   * The field "c" contains a hash of the contents of the message.
+   * This field is used to verify the integrity of the data upon decryption,
+   * as Kyber decryption does not throw an error if incorrect set of symmetric key + initvector
+   * is used. Instead it returns a wrong payload.
+   */
   h: string;
 }
